70e43f4a9e8d184e2a4d68dbd807432756e06a2d
Екатерина Чуквасова <e.chukvasova@yandex.ru>
2023-04-13 01:28:44 +0300

    yamdb_final workflow_v.1.26

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,10 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '84.201.129.162',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
